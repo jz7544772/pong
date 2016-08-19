518a1df0aae13b9be33cabfe2432a1ccdff496e2
Jason Guo <jiazheng.guo@mail.utoronto.ca>
2016-08-18 22:09:18 -0400

    Initialize repository

---
+++ WinformPong/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinformPong
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.stage = new System.Windows.Forms.Panel();
+            this.opponent = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.ballTimer = new System.Windows.Forms.Timer(this.components);
+            this.opponentTimer = new System.Windows.Forms.Timer(this.components);
+            this.stage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.opponent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stage
+            // 
+            this.stage.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.stage.Controls.Add(this.opponent);
+            this.stage.Controls.Add(this.player);
+            this.stage.Controls.Add(this.ball);
+            this.stage.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.stage.Location = new System.Drawing.Point(1, 1);
+            this.stage.Name = "stage";
+            this.stage.Size = new System.Drawing.Size(626, 200);
+            this.stage.TabIndex = 0;
+            // 
+            // opponent
+            // 
+            this.opponent.BackColor = System.Drawing.Color.Teal;
+            this.opponent.Location = new System.Drawing.Point(608, 69);
+            this.opponent.Name = "opponent";
+            this.opponent.Size = new System.Drawing.Size(15, 70);
+            this.opponent.TabIndex = 2;
+            this.opponent.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Orange;
+            this.player.Location = new System.Drawing.Point(0, 69);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(15, 70);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Black;
+            this.ball.InitialImage = null;
+            this.ball.Location = new System.Drawing.Point(296, 95);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(15, 15);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // ballTimer
+            // 
+            this.ballTimer.Enabled = true;
+            this.ballTimer.Interval = 1;
+            this.ballTimer.Tick += new System.EventHandler(this.ballTimer_Tick);
+            // 
+            // opponentTimer
+            // 
+            this.opponentTimer.Enabled = true;
+            this.opponentTimer.Tick += new System.EventHandler(this.opponentTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 203);
+            this.Controls.Add(this.stage);
+            this.Name = "MainForm";
+            this.Text = "WinformPong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyUp);
+            this.stage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.opponent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel stage;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer ballTimer;
+        private System.Windows.Forms.PictureBox opponent;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer opponentTimer;
+    }
+}
+
---
+++ WinformPong/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WinformPong.Properties;
+
+namespace WinformPong
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        PictureBox[] playerScore = new PictureBox[5]; 
+        PictureBox[] opponentScore = new PictureBox[5]; 
+        Color scoreColor = Color.Silver;
+        Random rng = new Random();
+        Boolean playerUp, playerDown = false;
+        Boolean ballGoingLeft = false;
+        Boolean gameOn = false;
+
+        int playerSpeed;
+        int opponentSpeed;
+        int ballSpeed;
+        int ballForce = 0;
+        int round = 0; 
+
+        public void ApplySettings()
+        {
+            playerSpeed = Properties.Settings.Default.PlayerSpeed;
+            opponentSpeed = Properties.Settings.Default.OpponentSpeed;
+            ballSpeed = Properties.Settings.Default.BallSpeed;
+            ballTimer.Interval = Properties.Settings.Default.TimerMovement;
+            opponentTimer.Interval = Properties.Settings.Default.TimerOpponent;
+        }
+
+        public Boolean CollisionLeft(PictureBox pb)
+        {
+            if(pb.Location.X <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean CollisionRight(PictureBox pb)
+        {
+            if(pb.Location.X + pb.Width >= stage.Width)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean CollisionUp(PictureBox pb)
+        {
+            if(pb.Location.Y <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean CollisionDown(PictureBox pb)
+        {
+            if(pb.Location.Y + pb.Height >= stage.Height)
+            {
+                return true; 
+            }
+            return false;
+        }
+
+        public Boolean CollisionOpponent(PictureBox pb)
+        {
+            if (pb.Bounds.IntersectsWith(opponent.Bounds))
+            {
+                PictureBox tempPb = new PictureBox();
+                tempPb.Size = new Size(1, 10);
+                tempPb.Location = new Point(opponent.Location.X -1, opponent.Location.Y);
+
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = 3;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = 2;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = 1;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = 0;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = -1;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = -2;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = -3;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean CollisionPlayer(PictureBox pb)
+        {
+            if(pb.Bounds.IntersectsWith(player.Bounds))
+            {
+                PictureBox tempPb = new PictureBox();
+                tempPb.Size = new Size(1, 10);
+                tempPb.Location = new Point(player.Location.X + player.Width, player.Location.Y);
+
+                if(ball.Bounds.IntersectsWith(tempPb.Bounds))
+                {
+                    ballForce = 3;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if(ball.Bounds.IntersectsWith(tempPb.Bounds)) {
+                    ballForce = 2;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds)) {
+                    ballForce = 1;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds)) {
+                    ballForce = 0;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds)) {
+                    ballForce = -1;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds)) {
+                    ballForce = -2;
+                    return true;
+                }
+                tempPb.Top += 10;
+                if (ball.Bounds.IntersectsWith(tempPb.Bounds)) {
+                    ballForce = -3;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int ReverseInt(int x, Boolean force = false, Boolean negative = false)
+        {
+            if(force)
+            {
+                if(negative)
+                {
+                    if(x > 0)
+                    {
+                        x = ~x + 1;
+                    }
+                }
+                else
+                {
+                    x = x - (x * 2);
+                }
+            }
+            else
+            {
+                if(x > 0)
+                {
+                    x = x - (x * 2);
+                }
+                else
+                {
+                    x = ~x + 1;
+                }
+            }
+            return x;
+        }
+
+        private void ballTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                ApplySettings();
+
+                if (gameOn)
+                {
+                    if (playerUp && !(CollisionUp(player)))
+                    {
+                        player.Top -= playerSpeed;
+                    }
+                    else if (playerDown && !(CollisionDown(player)))
+                    {
+                        player.Top += playerSpeed;
+                    }
+
+                    if (ballForce != 0)
+                    {
+                        ball.Top -= ballForce;
+                    }
+
+                    if (CollisionUp(ball) || CollisionDown(ball))
+                    {
+                        ballForce = ~ballForce + 1;
+                    }
+
+                    if (ballGoingLeft)
+                    {
+                        ball.Left -= ballSpeed;
+                        if (CollisionPlayer(ball) || CollisionLeft(ball))
+                        {
+                            ballGoingLeft = false;
+                            ball.Left += 6;
+                        }
+                    }
+                    else // Ball currently going right 
+                    {
+                        ball.Left += ballSpeed;
+                        if (CollisionOpponent(ball) || CollisionRight(ball))
+                        {
+                            ballGoingLeft = true;
+                            ball.Left -= 6;
+                        }
+                    }
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }            
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    playerUp = false;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    playerDown = false;
+                    break;
+            }
+        }
+
+        private void opponentTimer_Tick(object sender, EventArgs e)
+        {
+            if(gameOn)
+            {
+                if(opponent.Location.Y +28 < ball.Location.Y)
+                {
+                    if(!CollisionDown(opponent))
+                    {
+                        opponent.Top += opponentSpeed;
+                    }
+                }
+                else {
+                    if(!CollisionUp(opponent))
+                    {
+                        opponent.Top -= opponentSpeed;
+                    }
+                }
+            }
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    playerDown = false;
+                    playerUp = true;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    playerUp = false;
+                    playerDown = true;
+                    break;
+                case Keys.Space:
+                    gameOn = !(gameOn);
+                    break;
+            }
+        }
+    }
+}
